897e6bab32e290c52ae89ca877d9dabdb821f227
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-11-19 12:28:33 +0530

    V 1.00

--- plugins/misc.js
+++ plugins/misc.js
@@ -14,6 +14,7 @@ Sparky(
   }) => {
 
       const start = new Date().getTime();
+let pong = await m.reply("Checking Ping")
 const end = new Date().getTime();
 await m.reply(`Latency : ${end - start} ms`)
   })

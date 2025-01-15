6479799b9035cb1f4e6b37829d4c2b86db39fefe
sanuser777 <sanunufu@gmail.com>
2025-01-16 00:31:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const BOT_TOKEN = process.env.BOT_TOKEN || '';
-const OWNER = process.env.OWNER || '';
+const BOT_TOKEN = process.env.BOT_TOKEN || '7773195501:AAG7IYmh8YbqfXijknqwuxtLAHsu35YQS5s';
+const OWNER = process.env.OWNER || '@vallapanikkumpodamyre';
 const OWNER_ID = process.env.OWNER_ID || 'https://t.me/belladesuda';
 const GROUP = process.env.GROUP || 'https://whatsapp.com/channel/0029Va9ZOf36rsR1Ym7O2x00';
 
